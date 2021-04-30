c118645abde7345b0f3ae8d4c9612d05e2ccde6c
frankhart <siddharthadhar.soumen@srmuniv.edu.in>
2021-04-30 19:36:38 +0530

    Add basic linear layer

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
+from prayog.layers.layer import Layer
+import torch.nn as nn
+import torch
+from torch.nn.modules.linear import Linear
+
 import prayog.layers as layers
 
+
+model = nn.Sequential(
+    layers.Linear(in_features=1, out_features=10, layer_name="fc_1-"),
+    layers.Linear(in_features=10, out_features=10, layer_name="fc_2-"),
+    layers.Linear(in_features=10, out_features=1, layer_name="fc_3-")
+)
+
+print(model)
 
 layer = layers.Linear(
     in_features=10,
@@ -7,7 +20,5 @@ layer = layers.Linear(
     layer_name="fc",
     count=1
 )
-
-import torch
 
 print(layer(torch.randn(10)))
